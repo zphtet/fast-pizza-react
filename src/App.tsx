2756e1d079d4f68b33436348d3fa60f3151be78c
zphtet <zinphtet.215108@gmail.com>
2023-10-10 20:16:28 +0630

    install dependencies

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <p className="text-red-600 text-2xl">Fast Pizza</p>
+      <button className="btn btn-success">Click Me</button>
+    </div>
+  );
+}
+
+export default App;
